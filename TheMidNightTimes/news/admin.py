8beeab190d481dd7808c012620d2b551e802cb00
ingalevijay <vijay.ingale@calsoftinc.com>
2023-10-06 18:45:20 +0530

    Resiter model in admin.py

--- TheMidNightTimes/news/admin.py
+++ TheMidNightTimes/news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Search, SearchResults
 
-# Register your models here.
+
+admin.site.register(Search)
+admin.site.register(SearchResults)
+
--- TheMidNightTimes/news/models.py
+++ TheMidNightTimes/news/models.py
@@ -9,12 +9,10 @@ class Search(models.Model):
 
 
 class SearchResults(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     search = models.ForeignKey(Search, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True, blank=True)
     author = models.CharField(max_length=100, null=True, blank=True)
     title = models.CharField(max_length=500, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     date_published = models.DateTimeField(null=True, blank=True)
-
-
